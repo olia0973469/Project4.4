478359194d24ceabd23a5656f03b7a7ffe77c74a
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2023-10-12 14:09:24 +0300

    Add project files.

---
+++ Project4.4/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -8 - R)
+			y = -R;
+		else
+			if (-8 - R < x && x <= -8 + R)
+				y = sqrt(-x * x - 16 * x - 64 + R * R) - R;
+			else
+				if (-8 + R < x && x <= 2)
+					y = (-4 * R + 2 * x + 16 + R * x);
+				else
+					if (2 < x && x <= 6)
+						y = 0;
+					else
+						y = (x - 6) * (x - 6);
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
